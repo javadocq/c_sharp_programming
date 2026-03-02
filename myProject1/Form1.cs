d85aa3663fc737d68e8f5f428c7f07882c1285eb
javadocq <han18264@naver.com>
2026-03-02 18:54:53 +0900

    프로젝트 생성 및 프로그램 맛보기

---
+++ myProject1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myProject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 버튼1을 클릭했을 때, 실행 프로그램 작성
+            label1.Text = "안녕하세요. C#입니다."; // label1에 글자 출력
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 버튼2를 클릭했을 때, 실행 프로그램 작성
+            label1.Text = ""; // label1에 글자 제거
+        }
+    }
+}
