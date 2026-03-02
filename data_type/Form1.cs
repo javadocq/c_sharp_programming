a0dee93ac3f3adc304a72aa1b89f2c3533df8607
javadocq <han18264@naver.com>
2026-03-02 21:30:18 +0900

    데이터 타입

---
+++ data_type/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace data_type
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            int iValue = 100;
+            lblResult.Text = iValue.ToString(); // label에 int 값을 문자열로 변환하여 표시
+        }
+
+        private void btnFloat_Click(object sender, EventArgs e)
+        {
+            float fValue = 3.14f;
+            lblResult.Text = fValue.ToString(); // label에 float 값을 문자열로 변환하여 표시
+        }
+
+        private void btnString_Click(object sender, EventArgs e)
+        {
+            string sValue = "Hello, World!";
+            lblResult.Text = sValue;
+        }
+    }
+}
---
+++ data_type/README.md
@@ -0,0 +1,12 @@
+### 자료형 Data type
+
+- Bool : True, False
+- Byte : 0 ~ 255
+- Sbyte : -128 ~ 127
+- Int : -2,147,483,648 ~ 2,147,483,647
+- Uint : 0 ~ 4,294,967,295
+- Float : 소수점 7자리 실수 ex) 3.14f
+- Double : 소수점 16자리 실수 ex) 3.14
+- Char : 문자 ex) 'A', 'B', 'C'
+- String : 문자열 ex) "Hello", "World"
+- Object : 모든 자료형의 부모 클래스, 모든 자료형을 Object로 변환 가능
