cee03c61c701ab5fa8a5a1049f05acdbf6ab4ef7
javadocq <han18264@naver.com>
2026-03-06 17:23:44 +0900

    day23_method

---
+++ day23_method/MainApp.cs
@@ -0,0 +1,19 @@
+﻿namespace day23_method
+{
+    internal class MainApp
+    {
+        delegate void MyDelegate(int a);
+
+    static void Main(string[] args)
+        {
+            // 무명 메서드를 delegate 타입 변수에 할당
+            MyDelegate d = delegate (int p)
+                {
+                    Console.Write(p);
+                };
+            d(100);
+
+
+    }
+}
+}
